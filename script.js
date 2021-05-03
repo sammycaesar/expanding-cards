61ea2d19300ef9fd138839e7d310e67ed8641ebd
sammycaesar <samantha.caesar@bigpond.com>
2021-05-03 12:05:57 +0200

    adding all files

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Variables
 const panels = document.querySelectorAll(".panel");
 
+// When use clicks on a panel, then remove active classes then add the active class to the panel that is clicked
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
@@ -7,6 +9,7 @@ panels.forEach((panel) => {
   });
 });
 
+// The function to remove active classes
 function removeActiveClasses() {
   panels.forEach((panel) => {
     panel.classList.remove("active");
